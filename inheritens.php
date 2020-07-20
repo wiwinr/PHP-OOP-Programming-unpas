39d23d031672775b8bd9e5c660b2b95cc86068ba
test <devnoname0@gmail.com>
2020-07-20 18:31:44 +0700

    Inheritence

---
+++ inheritens.php
@@ -0,0 +1,59 @@
+<?php 
+//jualan produk
+//komik
+//game
+
+class Produk {
+
+	public 	$judul,
+			$penulis,
+			$penerbit,
+			$harga,
+			$jmlHalaman,
+			$waktumain,
+			$tipe;
+
+	public function __construct( $judul= "judul", $penulis = "penulis" , $penerbit = "Penerbit", $harga = 0, $jmlHalaman = 0, $waktumain =0, $tipe0){
+			$this->judul = $judul;
+			$this->penulis = $penulis;
+			$this->penerbit = $penerbit;
+			$this->harga = $harga;
+			$this->jmlHalaman = $jmlHalaman;
+			$this->waktumain = $waktumain;
+			$this->tipe = $tipe;
+	}
+
+	public function getLabel() {
+		return "$this->penulis, $this->penerbit";
+	}		
+
+	public function getinfolengkap(){
+		// Komik Naruto | Masashi Kisimoto, sonen Jump (Rp. 30000) - 100 Halaman.
+		$str = "{$this->tipe} : {$this->judul} | {$this->getLabel()}. (Rp/ {$this->harga})";
+		if( $this->tipe == "komik"){
+			$str .= " - {$this->jmlHalaman} Halaman.";
+		}else if($this->tipe == "Game"){
+			$str .=" - {$this->waktumain} jam.";
+		}
+		return $str;
+	}
+
+
+}
+
+class CetakInfoProduk {
+	public function cetak(Produk $produk){
+		$str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+		return $str;
+	}
+}
+
+
+$produk1 = new Produk("Naruto", "Masashi Kisimoto", "sonen Jump", 30000, 100, 0 ,"komik");
+$produk2 = new Produk("Uncherted", "Neil Druckmann", "Sonny Computer", 2500000, 0, 50, "Game" );
+
+
+
+echo $produk1->getinfolengkap();
+echo "<br>";
+echo $produk2->getinfolengkap();
