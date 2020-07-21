8781d67db5642d70dad2987bdb71d9187f20b2da
test <devnoname0@gmail.com>
2020-07-21 08:31:28 +0700

    fix inheritance

--- inheritens.php
+++ inheritens.php
@@ -9,31 +9,34 @@ class Produk {
 			$penulis,
 			$penerbit,
 			$harga,
-			$jmlHalaman,
+			$jmlhalaman,
 			$waktumain,
 			$tipe;
-
-	public function __construct( $judul= "judul", $penulis = "penulis" , $penerbit = "Penerbit", $harga = 0, $jmlHalaman = 0, $waktumain =0, $tipe0){
+			
+	public function __construct( $judul= "judul", $penulis = "penulis" , $penerbit = "Penerbit", $harga = 0, $jmlhalaman = 0, $waktumain = 0, $tipe){
 			$this->judul = $judul;
 			$this->penulis = $penulis;
 			$this->penerbit = $penerbit;
 			$this->harga = $harga;
-			$this->jmlHalaman = $jmlHalaman;
+			$this->jmlhalaman = $jmlhalaman;
 			$this->waktumain = $waktumain;
 			$this->tipe = $tipe;
+			
 	}
 
 	public function getLabel() {
 		return "$this->penulis, $this->penerbit";
 	}		
 
-	public function getinfolengkap(){
-		// Komik Naruto | Masashi Kisimoto, sonen Jump (Rp. 30000) - 100 Halaman.
-		$str = "{$this->tipe} : {$this->judul} | {$this->getLabel()}. (Rp/ {$this->harga})";
+	public function getlengkap(){
+
+		//komik : Naruto | Masashi kisimoto, Shonen Jump (Rp. 30000) - 100 Halaman.
+		$str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+
 		if( $this->tipe == "komik"){
-			$str .= " - {$this->jmlHalaman} Halaman.";
-		}else if($this->tipe == "Game"){
-			$str .=" - {$this->waktumain} jam.";
+			$str.= " - {$this->jmlhalaman} Halaman.";
+		}else if ( $this->tipe == "game"){
+			$str .= " ~ {this->waktumain} Jam.";
 		}
 		return $str;
 	}
@@ -49,11 +52,13 @@ class CetakInfoProduk {
 }
 
 
-$produk1 = new Produk("Naruto", "Masashi Kisimoto", "sonen Jump", 30000, 100, 0 ,"komik");
-$produk2 = new Produk("Uncherted", "Neil Druckmann", "Sonny Computer", 2500000, 0, 50, "Game" );
+$produk1 = new Produk("NAruto", "Masashi Kisimoto", "shonen Jump", 30000, 100, 0, "komik");
+$produk2 = new Produk("Uncherted", "Neil Druckmann", "Sonny Computer", 2500000, 0, 50, "game");
 
 
-
-echo $produk1->getinfolengkap();
+echo $produk1->getlengkap();
 echo "<br>";
-echo $produk2->getinfolengkap();
+echo $produk2->getlengkap();
+
+
+ ?>
