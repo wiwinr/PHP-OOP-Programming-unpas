d6645f1f0af660002d2d16d045fd6b46688ebe13
test <devnoname0@gmail.com>
2020-07-21 09:45:43 +0700

    Overiding, yaitu proses menimpa properti parrent yang tidak diguanakan oleh child

---
+++ overiding.php
@@ -0,0 +1,98 @@
+<?php 
+//jualan produk
+//komik
+//game
+
+class Produk {
+
+	public 	$judul,
+			$penulis,
+			$penerbit,
+			$harga;
+		
+			
+	public function __construct(
+		$judul= "judul",
+		$penulis = "penulis" ,
+		$penerbit = "Penerbit",
+		$harga = 0
+	){
+		$this->judul = $judul;
+		$this->penulis = $penulis;
+		$this->penerbit = $penerbit;
+		$this->harga = $harga;
+
+	}
+
+	
+	public function getLabel() {
+		return "$this->penulis, $this->penerbit";
+	}		
+
+	
+	public function getinfoproduk(){
+
+		$str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+		return $str;
+	}
+
+
+}
+
+
+
+class komik extends Produk{
+	public $jmlhalaman;
+
+	public function __construct(
+		$judul= "judul",
+		$penulis = "penulis" ,
+		$penerbit = "Penerbit",
+		$harga = 0,
+		$jmlhalaman
+	){
+		parent::__construct($judul, $penulis, $penerbit , $harga );
+
+		$this->jmlhalaman = $jmlhalaman;
+	}
+	public function getinfoproduk(){
+		$str = "komik : " . parent::getinfoproduk() ." - {$this->jmlhalaman} Halaman.";
+		return $str;
+	}
+}
+
+class game extends Produk{
+
+	public $waktumain;
+
+	public function __construct( $judul= "judul", $penulis = "penulis" , $penerbit = "Penerbit", $harga = 0,  $waktumain = 0){
+
+		parent::__construct($judul, $penulis, $penerbit , $harga );
+
+		$this->waktumain = $waktumain;
+	}
+
+	public function getinfoproduk(){
+		$str = "game : " . parent::getinfoproduk() ." ~ {$this->waktumain} Jam.";
+		return $str;
+
+	}
+}
+
+class CetakInfoProduk {
+	public function cetak(Produk $produk){
+		$str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+		return $str;
+	}
+}
+
+
+$produk1 = new komik("Naruto", "Masashi Kisimoto", "shonen Jump", 30000, 100);
+$produk2 = new game("Uncherted", "Neil Druckmann", "Sonny Computer", 2500000,50);
+
+echo $produk1->getinfoproduk();
+echo "<br>";
+echo $produk2->getinfoproduk();
+
+
+ ?>
